e1c7304864c6b36a0909b7650cda89b2dd2a6f9f
Евгений Кравцов < >
2017-05-25 23:43:38 +0200

    3.1.2 Zaprojektowanie klasy stack

--- src/main/java/edu/kis/vh/stacks/stack.java
+++ src/main/java/edu/kis/vh/stacks/stack.java
@@ -1,34 +1,39 @@
 package edu.kis.vh.stacks;
 
 public class stack {
-
-	private int[] ITEMS = new int[12];	
-
-	public int total = -1;
 	
+	private StackArray stackArray;
+
+	public stack(StackArray stackArray) {
+		super();
+		this.stackArray = stackArray;
+	}
+
+	public stack() {
+		super();
+		stackArray = new StackArray();
+	}
+
 	public void push(int i) {
-		if (!isFull())
-		ITEMS[++total] = i;
+		stackArray.push(i);
+	}
+
+	public boolean isEmpty() {
+		return stackArray.isEmpty();
+	}
+
+	public boolean isFull() {
+		return stackArray.isFull();
+	}
+
+	public int top() {
+		return stackArray.top();
+	}
+
+	public int pop() {
+		return stackArray.pop();
 	}
 	
-		public boolean isEmpty() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-			
-				public int top() {
-					if (isEmpty())
-						return -1;
-					return ITEMS[total];
-				}
-				
-					public int pop() {
-						if (isEmpty())
-							return -1;
-						return ITEMS[total--];
-					}
-				
+	
+	
 }
